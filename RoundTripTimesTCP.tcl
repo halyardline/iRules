761b7d55241c309dd4dfeac217d22893d1f95863
halyardline <24568806+halyardline@users.noreply.github.com>
2019-10-19 15:21:05 -0400

    Changed iRule
    Signed-off-by: halyardline <24568806+halyardline@users.noreply.github.com>

--- RoundTripTimesTCP.tcl
+++
@@ -1,5 +0,0 @@
-when SERVER_CONNECTED {
-   clientside { set clientrtt [expr {double([TCP::rtt]) / 32}] }
-   set serverrtt [expr {double([TCP::rtt]) / 32}]
-   log "Client RTT $clientrtt ms ; Server RTT $serverrtt"
-}
